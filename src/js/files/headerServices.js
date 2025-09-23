91b35ee60fa9b23d459445605455b2c72531148f
pauchok-sk <pauchoooooook@gmail.com>
2025-09-23 12:01:11 +0300

    category page

--- src/js/files/headerServices.js
+++ src/js/files/headerServices.js
@@ -10,4 +10,35 @@ export default function headerServices() {
       overlay.classList.remove("_active");
     });
   }
+
+  const buttonsServices = document.querySelectorAll("[data-service]");
+
+  if (buttonsServices.length) {
+    const lists = document.querySelectorAll("[data-service-list]");
+
+    buttonsServices.forEach((btn) => {
+      btn.addEventListener("mouseenter", () => {
+        if (btn.classList.contains("_active")) return;
+        
+        const id = btn.dataset.service;
+        const currentList = document.querySelector(
+          `[data-service-list="${id}"]`
+        );
+
+
+        lists.forEach((l) => {
+          l.classList.remove("_active");
+          l.style.opacity = 0;
+        });
+        buttonsServices.forEach((b) => b.classList.remove("_active"));
+
+        currentList.classList.add("_active");
+        btn.classList.add("_active");
+
+        setTimeout(() => {
+          currentList.style.opacity = "1";
+        }, 100);
+      });
+    });
+  }
 }
